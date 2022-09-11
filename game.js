34669694c923116b18998b1edbc0ba3b39010493
Babatunde <d2ndeayoade@gmail.com>
2022-09-11 22:54:50 +0100

    i added another const with the name (answerOptions) to the game.js file in other to fetch all the answer options from the paragraph element that contain the answers in game.html page using the paragraph elemnent ID tag

--- game.js
+++ game.js
@@ -1,2 +1,4 @@
 // To fetch question from the the h2 element
 const question = document.getElementById("question");
+// To fetch all answers
+const answerOptions = document.getElementsByClassName("answer-text");
