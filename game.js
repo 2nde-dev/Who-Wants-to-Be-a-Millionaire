7b8d0c2b6528fb4aca2f1f9202c9e2358f7cdf34
Babatunde <d2ndeayoade@gmail.com>
2022-09-12 00:01:45 +0100

    i added the first set of question and answers into the array (gameQuestions) as object so the array is no longer empty

--- game.js
+++ game.js
@@ -3,4 +3,13 @@ const question = document.getElementById("question");
 // To fetch all answers
 const answerOptions = document.getElementsByClassName("answer-text");
 
-let gameQuestions = [];
+let gameQuestions = [
+  {
+    question: "Which of these is the name of a British Football Club?",
+    option1: "Blackburn Losers",
+    option2: "Blackburn Rovers",
+    option3: "Blackburn Lovers",
+    option4: "Blackburn Wanderers",
+    answer: 2,
+  },
+];
